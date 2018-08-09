f032a9e35c06808b86e5cd80b9a188e239a367fe
Jürgen Enge <juergen.enge@fhnw.ch>
2018-08-09 10:11:45 +0200

    ion-buttons repariert

--- search.php
+++ search.php
@@ -309,8 +309,8 @@ if( $json ) {
 
 echo mediathekheader('search', 'mediathek - Suche - '.$qobj['query'], $qobj['area'], array(), $qconfig );
 ?>
-<div class="home-btn"><i class="ion-android-close"></i></div>
-<div class="setting-btn"><i class="<?php echo $session->isLoggedIn() ? 'ion-ios-settings-strong': 'ion-log-in'; ?>"></i></div>
+<div class="home-btn"><i class="ion-ios-close-circle-outline"></i></div>
+<div class="setting-btn"><i class="<?php echo $session->isLoggedIn() ? 'ion-ios-settings': 'ion-ios-log-in'; ?>"></i></div>
 <div id="fullsearch" class="fullsearch-page container-fluid page">
     <div class="row">
             <!--( a ) Profile Page Fixed Image Portion -->
